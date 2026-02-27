4087439492c152b64d15f685b4c65883c5089020
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-27 10:40:41 +0600

    Count.jsx components added

--- src/Components/Count.jsx
+++ src/Components/Count.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 
 const Count = () => {
-    // const [state, setState] = useState(initialState)
-    const [count,setCount] = useState(0)
-    // console.log(count);
-    const add = ()=>{
-        // alert("btn clicked")
-      const newCoutn =   count + 1;
-        setCount(newCoutn)
+    const [age,seAge] = useState(42);
+    const [name,setName] = useState('Taylor');
+    const handleClick = ()=>{
+        setName('Robin')
+    }
+    const update = ()=>{
+        seAge(age + 1);
     }
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={add}>Add</button>
+            <h1>Name:{name}</h1>
+            <button onClick={handleClick}>Set Name</button>
+            <button onClick={update}>You Pressed me {age} times</button>
         </div>
     );
 };
