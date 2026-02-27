3dccd8073059a48eb08527fb65590b8006f96c82
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-27 12:29:18 +0600

    asynic awite data fetch

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,14 +1,19 @@
 import React, { use } from 'react';
 
-const Users = ({allData}) => {
+const Users = ({ allData }) => {
     console.log(allData);
     const datas = use(allData)
     console.log(datas);
+
     return (
-        <div>
+
+        <div style={{
+            border:"2px solid green"
+        }}>
+           
             <h1>User Here:{datas.length}</h1>
             {
-                datas.map(({id,name,email})=>(
+                datas.map(({ id, name, email }) => (
                     <div key={id}>
                         <h3>{name}</h3>
                         <p>{email}</p>
