a8c3116c3a4c3bd95a1ac3129c47081ea4548644
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-27 09:32:11 +0600

    Today Commit

--- src/Components/Friends.jsx
+++ src/Components/Friends.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-
-const Friends = ({friend,showButton}) => {
+const Friends = ({friend}) => {
     const {id,name,age,hobbie} = friend;
-    const greating = ()=>{
-        alert("hi md")
-    }
-    // console.log(friend);
     return (
         <div>
-            {
-                showButton && (
-                     <button onClick={greating}>click me</button>
-                )
-            }
             <h1>Name:{name}</h1>
             <h1>age:{age}</h1>
             <h1>hobbie:{hobbie}</h1>
             <h1>id:{id}</h1><br />
-           
         </div>
     );
 };
-
 export default Friends;
