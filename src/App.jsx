020a8fd15069953f355403cf15ed08933480ac6d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-27 09:41:53 +0600

    event handle added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ const App = () => {
     {id:2,name:'ar',age:20,hobbie:'codding'},
     {id:3,name:'ar',age:20,hobbie:'codding'},
   ]
+  const handle = ()=>{
+    alert("btn clicked")
+  }
   return (
     <div>
       <h1>hi iam third react</h1>
       {
         myFriends.map(friend=><Friends key={friend.id}  friend={friend}></Friends>)
       }
+      <button onClick={handle}>Click Me</button>
     </div>
   );
 };
